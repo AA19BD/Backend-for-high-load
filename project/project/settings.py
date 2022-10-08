52854c9e21ede49c8dbd20e0f16d8b7eb09fd4e3
Abilay Aytbanov <aitbanov640@gmail.com>
2022-10-08 17:55:23 +0600

    Updated

--- project/project/settings.py
+++ project/project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ttjrgz$#adbz$4)(3qdcuwni9zk=j-=*_4zf*6yao-xf(7b!^@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0','localhost','127.0.0.1']
 
 
 # Application definition
